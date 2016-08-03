6fb4c541476e7d9a86e282aa93d774ab3af21eb4
Matthieu Grebot <matcoopa@gmail.com>
2016-08-03 15:37:49 +0100

    Review model added and associations tested

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803134341) do
+ActiveRecord::Schema.define(version: 20160803141035) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -22,5 +22,14 @@ ActiveRecord::Schema.define(version: 20160803134341) do
     t.datetime "updated_at",  null: false
     t.string   "chef"
   end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "restaurant_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "reviews", ["restaurant_id"], name: "index_reviews_on_restaurant_id"
 
 end
