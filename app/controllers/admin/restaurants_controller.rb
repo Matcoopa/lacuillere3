35108da61d23ae1518f603bcb178aa419f24594d
Matthieu Grebot <matcoopa@gmail.com>
2016-08-03 17:35:01 +0100

    VAlidations in place

---
+++ app/controllers/admin/restaurants_controller.rb
@@ -0,0 +1,8 @@
+class Admin::RestaurantsController < ApplicationController
+
+  def index
+    @restaurants =Restaurant.all
+  end
+
+end
+
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,10 @@
 class Restaurant < ActiveRecord::Base
+  validates :stars, inclusion: { in: [1,2,3], allow_nil: false }
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+
   has_many :reviews, dependent: :destroy
+
+
+
 end
